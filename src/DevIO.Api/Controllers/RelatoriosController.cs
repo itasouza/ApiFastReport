1721a29e040c6781732c2b3b2fc9cee62d95e0d8
itasouza <itasouza@yahoo.com.br>
2021-08-24 20:45:24 -0300

    Criação do HelperFastReport

--- src/DevIO.Api/Controllers/RelatoriosController.cs
+++ src/DevIO.Api/Controllers/RelatoriosController.cs
@@ -1,13 +1,8 @@
 ﻿using DevIO.Api.Data;
-using FastReport.Export.PdfSimple;
-using FastReport.Web;
+using DevIO.Api.Helper;
 using Microsoft.AspNetCore.Mvc;
-using System;
-using System.Collections.Generic;
 using System.Data;
-using System.IO;
 using System.Linq;
-using System.Threading.Tasks;
 
 namespace DevIO.Api.Controllers
 {
@@ -28,30 +23,20 @@ namespace DevIO.Api.Controllers
                 return BadRequest(ModelState);
             }
 
-            var webReport = new WebReport();
-            webReport.Report.Load(Path.Combine("Reports\\ListagemUsuario.frx"));
+            var webReport = HelperFastReport.WebReport("ListagemUsuario.frx");
+
             var usuarioList = _context.Usuarios.Where(u => u.Nome != "").ToList();
             var usuarios = new DataTable();
             usuarios.Columns.Add("id", typeof(string));
             usuarios.Columns.Add("Nome", typeof(string));
             usuarios.Columns.Add("Email", typeof(string));
-
             foreach (var item in usuarioList)
             {
                 usuarios.Rows.Add(item.Id, item.Nome, item.Email);
             }
             webReport.Report.RegisterData(usuarios, "Usuarios");
-            webReport.Report.Prepare();
-            byte[] reportArray = null;
-            using (MemoryStream ms = new MemoryStream())
-            {
-                var pdfExport = new PDFSimpleExport();
-                pdfExport.Export(webReport.Report, ms);
-                ms.Flush();
-                reportArray = ms.ToArray();
 
-            }
-            return File(reportArray, "application/pdf", "ListagemDeUsuario.pdf");
+            return File(HelperFastReport.ExportarPdf(webReport), "application/pdf", "ListagemDeUsuario.pdf");
         }
 
 
---
+++ src/DevIO.Api/Helper/HelperFastReport.cs
@@ -0,0 +1,34 @@
+﻿using FastReport.Export.PdfSimple;
+using FastReport.Web;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DevIO.Api.Helper
+{
+    public static class HelperFastReport
+    {
+        public static WebReport WebReport(string nomeDoRelatorio)
+        {
+            var result = new WebReport();
+            result.Report.Load(Path.Combine("Reports", nomeDoRelatorio));
+            return result;
+        }
+
+        public static byte[] ExportarPdf(WebReport webReport)
+        {
+            webReport.Report.Prepare();
+            using (MemoryStream ms = new MemoryStream())
+            {
+                var pdfExport = new PDFSimpleExport();
+                pdfExport.Export(webReport.Report, ms);
+                ms.Flush();
+                return ms.ToArray();
+
+            }
+        }
+
+    }
+}
