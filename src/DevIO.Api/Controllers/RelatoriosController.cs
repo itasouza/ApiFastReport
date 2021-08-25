125a8e86b90f84daa7c3472fad421136957e7a50
itasouza <itasouza@yahoo.com.br>
2021-08-24 21:25:07 -0300

    Criado o GetTable para transformar uma lista em um DataTable

--- src/DevIO.Api/Controllers/RelatoriosController.cs
+++ src/DevIO.Api/Controllers/RelatoriosController.cs
@@ -1,4 +1,5 @@
 ﻿using DevIO.Api.Data;
+using DevIO.Api.Entity;
 using DevIO.Api.Helper;
 using Microsoft.AspNetCore.Mvc;
 using System.Data;
@@ -26,19 +27,22 @@ namespace DevIO.Api.Controllers
             var webReport = HelperFastReport.WebReport("ListagemUsuario.frx");
 
             var usuarioList = _context.Usuarios.Where(u => u.Nome != "").ToList();
-            var usuarios = new DataTable();
-            usuarios.Columns.Add("id", typeof(string));
-            usuarios.Columns.Add("Nome", typeof(string));
-            usuarios.Columns.Add("Email", typeof(string));
-            foreach (var item in usuarioList)
-            {
-                usuarios.Rows.Add(item.Id, item.Nome, item.Email);
-            }
+            var usuarios = HelperFastReport.GetTable<UsuarioEntity>(usuarioList, "Usuarios");
             webReport.Report.RegisterData(usuarios, "Usuarios");
-
+            //"Usuarios" nome usado no relatório como Data Source
             return File(HelperFastReport.ExportarPdf(webReport), "application/pdf", "ListagemDeUsuario.pdf");
         }
 
 
     }
 }
+
+//exemplo usando o modelo comum 
+//var usuarios = new DataTable();
+//usuarios.Columns.Add("id", typeof(string));
+//usuarios.Columns.Add("Nome", typeof(string));
+//usuarios.Columns.Add("Email", typeof(string));
+//foreach (var item in usuarioList)
+//{
+//    usuarios.Rows.Add(item.Id, item.Nome, item.Email);
+//}
